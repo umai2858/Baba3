b07f25c4ddd3746325f11cb5be239618eca933e7
BOT_RASHMIKA <janith60022006@gmail.com>
2024-11-07 02:25:41 -0800

    QUEEN ANJU MD 😍

---
+++ config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  "SESSION_ID": "add your session id here",
+  "ALIVE_IMG": "https://raw.githubusercontent.com/Mrrashmika/Database/refs/heads/main/WhatsApp%20Image%202024-09-08%20at%209.00.17%20PM.jpeg",
+  "PREFIX": ".",
+  "AUTO_READ_STATUS": true,
+  "MODE": "public",
+  "BOT_NUMBER": "94717775628",
+  "OMDB_API_KEY": "76cb7f39",
+  "READ_CMD": true,
+  "AUTO_VOICE": true,
+  "AUTO_STICKER": false,
+  "AUTO_REPLY": true,
+  "AUTO_REACT": false,
+  "WELCOME": false,
+  "ANTI_BAD": true,
+  "ANTI_BOT": true,
+  "ANTI_LINK": true,
+  "ALLWAYS_ONLINE": true,
+  "MOROCCO_BLOCK": true
+};
